adbd0f67ac7e7ae7826a40e0a97780086937b73c
Darra Funna <59220475+darrafunna@users.noreply.github.com>
2021-12-18 17:46:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ document.getElementById("buttonID").onclick = function(){
     var nama = document.getElementById("namaID").value;
     var kategori = document.getElementById("kategoriID").value;
     var jumlahPemakaian = document.getElementById("pemakaianID").value;
-    var tarif = 0;
-    var abodemen = 0 ;
     var pajak = 0;
     var subtotal = 0;
     var tabel = '';
@@ -21,41 +19,74 @@ document.getElementById("buttonID").onclick = function(){
     document.getElementById("outputJumlah").innerHTML = jumlahPemakaian;
 
     if (kategori == "Sosial"){
-        abodemen = 10000;
-        tarif = 300;
+        const sosial = [300, 10000];
+        for (var i = 1; i <= jumlahPemakaian ; i++){
+            subtotal = sosial[0] + sosial[1];
+            tabel += "<tr>";
+            tabel += "<td>" + i + "</td>";
+            tabel += "<td>" + sosial[0] + "</td>";
+            tabel += "<td>" + sosial[1] + "</td>"; 
+            tabel += "<td>" + subtotal + "</td>"; 
+            tabel += "</tr>"
+            sosial[0] += sosial[0];
+        }
         pajak = 0;
     }
     else if (kategori == "Rumah"){
-        abodemen = 30000;
-        tarif = 500;
+        const rumah = [500, 30000];
+        for (var i = 1; i <= jumlahPemakaian ; i++){
+            subtotal = rumah[0] + rumah[1];
+            tabel += "<tr>";
+            tabel += "<td>" + i + "</td>";
+            tabel += "<td>" + rumah[0] + "</td>";
+            tabel += "<td>" + rumah[1] + "</td>"; 
+            tabel += "<td>" + subtotal + "</td>"; 
+            tabel += "</tr>"
+            rumah[0] += rumah[0];
+        }
         pajak = 0.1;
     }
     else if (kategori == "Apartemen"){
-        abodemen = 50000;
-        tarif = 750;
+        const apart = [750, 50000];
+        for (var i = 1; i <= jumlahPemakaian ; i++){
+            subtotal = apart[0] + apart[1];
+            tabel += "<tr>";
+            tabel += "<td>" + i + "</td>";
+            tabel += "<td>" + apart[0] + "</td>";
+            tabel += "<td>" + apart[1] + "</td>"; 
+            tabel += "<td>" + subtotal + "</td>"; 
+            tabel += "</tr>"
+            apart[0] += apart[0];
+        }
         pajak = 0.15;
     }
     else if (kategori == "Industri"){
-        abodemen = 75000;
-        tarif = 1000;
+        const industri = [1000, 75000];
+        for (var i = 1; i <= jumlahPemakaian ; i++){
+            subtotal = industri[0] + industri[1];
+            tabel += "<tr>";
+            tabel += "<td>" + i + "</td>";
+            tabel += "<td>" + industri[0] + "</td>";
+            tabel += "<td>" + industri[1] + "</td>"; 
+            tabel += "<td>" + subtotal + "</td>"; 
+            tabel += "</tr>"
+            industri[0] += industri[0];
+        }
         pajak = 0.2;
     }
     else {
-        abodemen = 100000;
-        tarif = 1250;
+        const villa = [1250, 100000];
+        for (var i = 1; i <= jumlahPemakaian ; i++){
+            subtotal = villa[0] + villa[1];
+            tabel += "<tr>";
+            tabel += "<td>" + i + "</td>";
+            tabel += "<td>" + villa[0] + "</td>";
+            tabel += "<td>" + villa[1] + "</td>"; 
+            tabel += "<td>" + subtotal + "</td>"; 
+            tabel += "</tr>"
+            villa[0] += villa[0];
+        }
         pajak = 0.25;
-    }
-
-
-    for (var i = 1; i <= jumlahPemakaian ; i++){
-        subtotal = tarif + abodemen;
-        tabel += "<tr>";
-        tabel += "<td>" + i + "</td>";
-        tabel += "<td>" + tarif + "</td>";
-        tabel += "<td>" + abodemen + "</td>"; 
-        tabel += "<td>" + subtotal + "</td>"; 
-        tabel += "</tr>"
-        tarif += tarif;
     }
     
     document.getElementById("tabelID").innerHTML = tabel;
